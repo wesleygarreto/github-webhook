fed5d291662c59e4d0408de5b531e10d762d8c0e
Wesley Garreto <wesley_garreto@hotmail.com>
2024-10-06 18:17:46 -0300

    Estrutura inicial

---
+++ src/main/java/com/garreto/githubwebhook/GithubWebhookApplication.java
@@ -0,0 +1,13 @@
+package com.garreto.githubwebhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubWebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubWebhookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garreto/githubwebhook/GithubWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garreto.githubwebhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubWebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
